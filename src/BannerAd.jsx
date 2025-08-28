2e1580565e80126b26d91ae8ddd4b2237c1b4c70
Mohamed Tabiyan Jagirdar <tabiyanads@gmail.com>
2025-08-28 17:57:27 +0530

    Add BannerAd.jsx and Jobs.jsx and index.html updated

--- src/BannerAd.jsx
+++ src/BannerAd.jsx
@@ -2,18 +2,12 @@ import { useEffect } from "react";
 
 export default function BannerAd() {
     useEffect(() => {
-        // Create inline script to inject HilltopAds
+        // HilltopAds script injection
         const script = document.createElement("script");
-        script.innerHTML = `(function(uq){
-      var d=document,
-          s=d.createElement('script'),
-          l=d.scripts[d.scripts.length-1];
-      s.settings=uq||{};
-      s.src="//fancyresponse.com/bPXKVGs.dSGrlO0GYmW/cG/_eFmu9/u/ZpUmlDk/PcT_Y/2BMvzOAK3LM/zkgYtuNlj/YlzVMdDAcPzcOmQL";
-      s.async=true;
-      s.referrerPolicy='no-referrer-when-downgrade';
-      l.parentNode.insertBefore(s,l);
-    })({})`;
+        script.src =
+            "//fancyresponse.com/bPXKVGs.dSGrlO0GYmW/cG/_eFmu9/u/ZpUmlDk/PcT_Y/2BMvzOAK3LM/zkgYtuNlj/YlzVMdDAcPzcOmQL";
+        script.async = true;
+        script.referrerPolicy = "no-referrer-when-downgrade";
 
         document.body.appendChild(script);
 
@@ -24,7 +18,6 @@ export default function BannerAd() {
 
     return (
         <div className="w-full flex justify-center p-4 bg-gray-50">
-            {/* HilltopAds script will inject ad here */}
             <div id="hilltop-ads-banner" className="max-w-screen-md" />
         </div>
     );
