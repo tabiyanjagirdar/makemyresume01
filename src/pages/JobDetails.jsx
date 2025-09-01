280ed1d40a4ee2ed9785d4e897b2b2d24e2b21f4
Mohamed Tabiyan Jagirdar <tabiyanads@gmail.com>
2025-09-01 19:19:58 +0530

    Add applynowad image, remove NativeAd, update JobDetails

--- src/pages/JobDetails.jsx
+++ src/pages/JobDetails.jsx
@@ -97,7 +97,7 @@ export default function JobDetails() {
                 <div className="flex justify-center mb-6">
                     <div className="bg-white shadow-md rounded-2xl p-3 border border-gray-200">
                         {/* Clickable image */}
-                        <a href="https://www.revenuecpmgate.com/cursb43u?key=3286d3862d0ba4b74b875af9c4c51b42">
+                        <a href="https://www.revenuecpmgate.com/hpppsb5n7r?key=9dfa5af7ea37a41696d231fc108bdbed">
                             <img
                                 src={applyNowAd}
                                 alt="Advertisement"
