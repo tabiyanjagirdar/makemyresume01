88947c429c5bb7e53e52ee744d1ad43739ce28c4
Mohamed Tabiyan Jagirdar <tabiyanads@gmail.com>
2025-09-12 16:31:58 +0530

    Updated CourseDetails, JobDetails, and Jobs pages

--- src/pages/JobDetails.jsx
+++ src/pages/JobDetails.jsx
@@ -198,7 +198,7 @@ export default function JobDetails() {
                     </a>
                 </div>
 
-                {/* <ScrollAd /> */}
+                <ScrollAd />
 
             </div>
         </div>
